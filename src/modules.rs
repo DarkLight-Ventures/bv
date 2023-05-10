03113e7e5ff8c92f5272e8bd2fc3e2f6c0b54f56
Kelsea Blackwell <kelsea@darklightventures.com>
2023-05-10 10:32:34 -0400

    stubbing in modules

---
+++ src/modules.rs
@@ -0,0 +1,17 @@
+/*
+ *  Copyright (c) 2023 Darklight Ventures
+ */
+
+use anyhow::Result;
+use tracing::{debug};
+use crate::cli::CliArgs;
+
+pub fn add_module(args: &CliArgs) -> Result<()> {
+    debug!("add_module placeholder!");
+    Ok()
+}
+
+pub fn auto_add_modules(args: &CliArgs)  -> Result<()> {
+    debug!("auto_add_placeholder!");
+    Ok()
+}
