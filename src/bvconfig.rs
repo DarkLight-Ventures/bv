a062206a7853304cd7cbdf60ef766b193fc8c79d
Kelsea Blackwell <kelsea@darklightventures.com>
2023-05-09 22:04:25 -0400

    added structures for handling semantic versioning

--- src/bvconfig.rs
+++ src/bvconfig.rs
@@ -3,7 +3,9 @@
  */
 
 use anyhow::Result;
-use serde::{Deserialize, Serialize};
+use serde::{Serialize, Deserialize, Deserializer};
+use serde::ser::{Serializer};
+use serde::de::Error;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::PathBuf;
@@ -27,8 +29,81 @@ pub struct BvConfig {
 pub struct ModuleConfig {
     pub name: Option<String>,
     pub current_version: Option<String>,
+    pub files: Option<Vec<ModuleFile>>,
 }
 
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub struct ModuleFile {
+    pub path: PathBuf,
+    pub replace_pattern: Option<String>,
+    pub search_pattern: Option<String>,
+    pub version: FileVersion,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub enum FileVersion {
+    Semantic(SemanticVersion),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct SemanticVersion {
+    major: i16,
+    minor: i16,
+    patch: i16,
+    pre_release: Option<String>,
+    build: Option<String>,
+}
+
+impl<'de> Deserialize<'de> for SemanticVersion {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let mut parts = s.split('+');
+        let pre_release_and_rest = parts.next().unwrap();
+        let build = parts.next().map(|s| s.to_string());
+
+        if parts.next().is_some() {
+            return Err(D::Error::custom("Too many '+' symbols in version string"));
+        }
+
+        let mut parts = pre_release_and_rest.split('-');
+        let version_and_rest = parts.next().unwrap();
+        let pre_release = parts.next().map(|s| s.to_string());
+
+        if parts.next().is_some() {
+            return Err(D::Error::custom("Too many '-' symbols in version string"));
+        }
+
+        let split: Vec<&str> = version_and_rest.split('.').collect();
+        if split.len() != 3 {
+            return Err(D::Error::custom("Expected format: [0-9]+.[0-9]+.[0-9]+"));
+        }
+
+        let major: i16 = split[0].parse().map_err(D::Error::custom)?;
+        let minor: i16 = split[1].parse().map_err(D::Error::custom)?;
+        let patch: i16 = split[2].parse().map_err(D::Error::custom)?;
+
+        Ok(Self { major, minor, patch, pre_release, build })
+    }
+}
+
+impl Serialize for SemanticVersion {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut version_str = format!("{}.{}.{}", self.major, self.minor, self.patch);
+        if let Some(pre_release) = &self.pre_release {
+            version_str = format!("{}-{}", version_str, pre_release);
+        }
+        if let Some(build) = &self.build {
+            version_str = format!("{}+{}", version_str, build);
+        }
+        serializer.serialize_str(&version_str)
+    }
+}
 
 impl Default for BvConfig {
     fn default() -> Self {
